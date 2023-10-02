fcdd410bea71579d60531c1fe0e7cc1e2719d3ed
Mathias Ulrich <Ullemanden@hotmail.com>
2023-10-02 09:57:27 +0200

    Første udkast

---
+++ script.js
@@ -0,0 +1,53 @@
+const board = document.getElementById("gameBoard");
+const currentPlayerEl = document.getElementById("currentPlayer");
+const winnerEl = document.getElementById("winner");
+const gameTimeEl = document.getElementById("gameTime");
+const turnCountEl = document.getElementById("turnCount");
+
+let gameArray = Array(6)
+  .fill()
+  .map(() => Array(7).fill(null));
+let currentPlayer = 1;
+let turnCount = 0;
+let gameStartTimestamp;
+
+function initGame() {
+  for (let i = 0; i < 6; i++) {
+    let row = board.insertRow();
+    for (let j = 0; j < 7; j++) {
+      let cell = row.insertCell();
+      cell.addEventListener("click", () => placeDisc(j));
+    }
+  }
+  gameStartTimestamp = new Date().getTime();
+  setInterval(updateTime, 1000);
+}
+
+function updateTime() {
+  const elapsedSeconds = Math.floor(
+    (new Date().getTime() - gameStartTimestamp) / 1000
+  );
+  gameTimeEl.textContent = elapsedSeconds;
+}
+
+function placeDisc(col) {
+  for (let row = 5; row >= 0; row--) {
+    if (!gameArray[row][col]) {
+      gameArray[row][col] = currentPlayer;
+      board.rows[row].cells[col].classList.add(`player${currentPlayer}`);
+      turnCount++;
+      checkWin(row, col);
+      currentPlayer = currentPlayer === 1 ? 2 : 1;
+      currentPlayerEl.textContent = currentPlayer;
+      turnCountEl.textContent = turnCount;
+      return;
+    }
+  }
+}
+
+function checkWin(row, col) {
+  // Tjek for vinder eller uafgjort her...
+  // For enkelheds skyld vil jeg ikke tilføje hele vinderlogikken her, men det kan tilføjes efter behov.
+}
+
+initGame();
