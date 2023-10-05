6b17a9b4087b3e5597bf7b2158e3d7105703678d
Mathias Ulrich <Ullemanden@hotmail.com>
2023-10-05 13:34:48 +0200

    Starfish animations switch

--- script.js
+++ script.js
@@ -14,6 +14,8 @@ let isGameComplete = false;
 
 let playerCount = parseInt(localStorage.getItem("playerCount"), 10) || 2; // default value
 
+const GIF_DURATION = 1100; // 1,1 seconds, adjust if the gif duration is different
+
 function initGame() {
   const rows =
     playerCount === 3 ? Math.ceil(6 * 1.5) : playerCount === 4 ? 6 * 2 : 6;
@@ -35,6 +37,36 @@ function initGame() {
   timerInterval = setInterval(updateTime, 1000);
 }
 
+function updateStarfishGif() {
+  const starfishEl = document.querySelector(
+    'img[src*="Starfish_Default_web.gif"]'
+  );
+  if (!starfishEl) return;
+
+  let newGifSrc = "";
+  switch (currentPlayer) {
+    case 1:
+      newGifSrc = "img/starfish_animations/Starfish_Player_1_web.gif";
+      break;
+    case 2:
+      newGifSrc = "img/starfish_animations/Starfish_Player_2_web.gif";
+      break;
+    case 3:
+      newGifSrc = "img/starfish_animations/Starfish_Player_3_web.gif";
+      break;
+    case 4:
+      newGifSrc = "img/starfish_animations/Starfish_Player_4_web.gif";
+      break;
+  }
+
+  if (newGifSrc) {
+    starfishEl.src = newGifSrc;
+    setTimeout(() => {
+      starfishEl.src = "img/starfish_animations/Starfish_Default_web.gif";
+    }, GIF_DURATION);
+  }
+}
+
 function updateTime() {
   //få fat i et felt på modal hvor man gerne ville skrive ud hvor lang tid spillet tog
   //der er 5 mulige scenariere for hvilken modal der udløses så derfor hentes der 5 felter
@@ -47,8 +79,7 @@ function updateTime() {
   let elapsedSeconds = 0;
 
   //tæl i sekunder mens spillet erigang
-  if(isGameComplete != true)
-  {
+  if (isGameComplete != true) {
     //elapsed seconds is updated here
     elapsedSeconds = Math.floor(
       (new Date().getTime() - gameStartTimestamp) / 1000
@@ -65,8 +96,7 @@ function updateTime() {
   totalTime5.textContent = `Spillet varede ${gameTimeEl.textContent} sekunder og spillet sluttede efter ${turnCountEl.textContent} runde`;
 
   //stop timer når spillet er afgjort (når en vinder eller spillet bliver uafgjort)
-  if(isGameComplete == true)
-  {
+  if (isGameComplete == true) {
     clearInterval(timerInterval);
   }
 }
@@ -111,6 +141,7 @@ function placeDisc(col) {
       checkWin(row, col);
       currentPlayer = (currentPlayer % playerCount) + 1;
       currentPlayerEl.textContent = currentPlayer;
+      updateStarfishGif(); // Update the starfish gif based on the currentPlayer
       //print antal ture på skærmen
       turnCountEl.textContent = turnCount;
       return;
@@ -124,85 +155,77 @@ function checkWin(row, col) {
   // Tjek for vinder eller uafgjort her...
   // Vi opretter en liste af retninger, vi skal tjekke for fire på stribe.
   const directions = [
-      { row: -1, col: 0 }, // lodret (opad)
-      { row: 0, col: 1 }, // vandret (højre)
-      { row: 1, col: 1 }, // diagonalt (nedad mod højre)
-      { row: 1, col: -1 } // diagonalt (nedad mod venstre)
+    { row: -1, col: 0 }, // lodret (opad)
+    { row: 0, col: 1 }, // vandret (højre)
+    { row: 1, col: 1 }, // diagonalt (nedad mod højre)
+    { row: 1, col: -1 }, // diagonalt (nedad mod venstre)
   ];
   // Vi gemmer den aktuelle spiller, som vi vil tjekke for en vinder.
   const playerToCheck = gameArray[row][col];
   // Nu går vi igennem hver retning for at se, om der er fire på stribe.
-  for(const dir of directions)
-  {
-      let count = 1; // Vi starter med en, da vi allerede har den aktuelle brik.
-      // Tjek i positiv retning (fremad i retningen).
-      for(let i = 1; i < 4; i++) 
-      {
-          const newRow = row + dir.row * i;
-          const newCol = col + dir.col * i;
-          // Vi tjekker, om den nye position er inden for brættet og om brikken er den samme som den aktuelle spillers.
-          if(
-            newRow >= 0 &&
-            newRow < gameArray.length &&
-            newCol >= 0 &&
-            newCol < gameArray[0].length &&
-            gameArray[newRow][newCol] === playerToCheck
-          ) {
-              count++;
-          }
-          else {
-            break;
-          }
+  for (const dir of directions) {
+    let count = 1; // Vi starter med en, da vi allerede har den aktuelle brik.
+    // Tjek i positiv retning (fremad i retningen).
+    for (let i = 1; i < 4; i++) {
+      const newRow = row + dir.row * i;
+      const newCol = col + dir.col * i;
+      // Vi tjekker, om den nye position er inden for brættet og om brikken er den samme som den aktuelle spillers.
+      if (
+        newRow >= 0 &&
+        newRow < gameArray.length &&
+        newCol >= 0 &&
+        newCol < gameArray[0].length &&
+        gameArray[newRow][newCol] === playerToCheck
+      ) {
+        count++;
+      } else {
+        break;
       }
-      // Tjek i negativ retning (baglæns i retningen).
-      for(let i = 1; i < 4; i++) {
-          const newRow = row - dir.row * i;
-          const newCol = col - dir.col * i;
-          if (
-            newRow >= 0 &&
-            newRow < gameArray.length &&
-            newCol >= 0 &&
-            newCol < gameArray[0].length &&
-            gameArray[newRow][newCol] === playerToCheck
-          ) {
-          count++;
-          }
-          else {
-            break;
-          }
+    }
+    // Tjek i negativ retning (baglæns i retningen).
+    for (let i = 1; i < 4; i++) {
+      const newRow = row - dir.row * i;
+      const newCol = col - dir.col * i;
+      if (
+        newRow >= 0 &&
+        newRow < gameArray.length &&
+        newCol >= 0 &&
+        newCol < gameArray[0].length &&
+        gameArray[newRow][newCol] === playerToCheck
+      ) {
+        count++;
+      } else {
+        break;
       }
-      // Hvis vi har fundet fire brikker på stribe i en retning, har vi en vinder.
-      if(count >= 4) {
-          //hver spiller får en unik predefineret victory screen og checkes der hvilken spiller der vandt
-          //og så åbnes der en bestemt modal der ønsker spilleren tilykke med sejren
-          if(playerToCheck === 1){
-              //når spiller 1 har 4 på stribe sættes isGameComplete til true
-              isGameComplete = true;
-              player1WinsModal();
-          }
-          else if(playerToCheck === 2){
-              //når spiller 2 har 4 på stribe sættes isGameComplete til true
-              isGameComplete = true;
-              player2WinsModal();
-          }
-          else if(playerToCheck === 3){
-              //når spiller 2 har 4 på stribe sættes isGameComplete til true
-              isGameComplete = true;
-              player3WinsModal();
-          }
-          else if(playerToCheck === 4){
-              //når spiller 2 har 4 på stribe sættes isGameComplete til true
-              isGameComplete = true;
-              player4WinsModal();
-          }
+    }
+    // Hvis vi har fundet fire brikker på stribe i en retning, har vi en vinder.
+    if (count >= 4) {
+      //hver spiller får en unik predefineret victory screen og checkes der hvilken spiller der vandt
+      //og så åbnes der en bestemt modal der ønsker spilleren tilykke med sejren
+      if (playerToCheck === 1) {
+        //når spiller 1 har 4 på stribe sættes isGameComplete til true
+        isGameComplete = true;
+        player1WinsModal();
+      } else if (playerToCheck === 2) {
+        //når spiller 2 har 4 på stribe sættes isGameComplete til true
+        isGameComplete = true;
+        player2WinsModal();
+      } else if (playerToCheck === 3) {
+        //når spiller 2 har 4 på stribe sættes isGameComplete til true
+        isGameComplete = true;
+        player3WinsModal();
+      } else if (playerToCheck === 4) {
+        //når spiller 2 har 4 på stribe sættes isGameComplete til true
+        isGameComplete = true;
+        player4WinsModal();
       }
+    }
   }
   //hvis pladen bliver helt fyldt med brikker så ender spillet uafgjort
-  if(turnCount === gameArray.length * gameArray[0].length)
-  {
-      //IsGameComplete sættes til true hvis spillet ender uafgjort
-      isGameComplete = true;
-      drawModal();
+  if (turnCount === gameArray.length * gameArray[0].length) {
+    //IsGameComplete sættes til true hvis spillet ender uafgjort
+    isGameComplete = true;
+    drawModal();
   }
 }
 //her laves en modal som vises hvis spillet bliver uafgjort
@@ -214,19 +237,19 @@ function drawModal() {
   drawModal.style.display = "block";
 
   function closeModal() {
-      //modal sættes til at være skjult når brugeren trykker på nyt spil knappen eller luk knappen
-      drawModal.style.display = "none";
-      window.location.href = "start.html";
+    //modal sættes til at være skjult når brugeren trykker på nyt spil knappen eller luk knappen
+    drawModal.style.display = "none";
+    window.location.href = "start.html";
   }
 
   //event listener på siden der lytter efter et click event
   closeDrawModalButton.addEventListener("click", closeModal);
   window.addEventListener("click", function (event) {
-  //hvis det var denne bestemte modal der triggered et click event så kald closeModal()
-  if(event.target === drawModal) {
+    //hvis det var denne bestemte modal der triggered et click event så kald closeModal()
+    if (event.target === drawModal) {
       //lukker modallen kaldet drawModal
       closeModal();
-  }
+    }
   });
 
   //En knap der lukker modallen og navigerer spillerne tilbage til hovedmenuen
@@ -241,20 +264,20 @@ function player1WinsModal() {
   player1WinsModal.style.display = "block";
 
   function closeModal1() {
-      //invoke this function when clicking a button in html
-      player1WinsModal.style.display = "none";
-      window.location.href = "start.html";
+    //invoke this function when clicking a button in html
+    player1WinsModal.style.display = "none";
+    window.location.href = "start.html";
   }
 
   closePlayer1WinsModalButton.addEventListener("click", closeModal1);
   window.addEventListener("click", function (event) {
-  if(event.target === player1WinsModal) {
+    if (event.target === player1WinsModal) {
       closeModal1();
-  }
+    }
   });
 
   const closeButton = document.querySelector(".closeModalButton");
-    closeButton.onclick = closeModal1;
+  closeButton.onclick = closeModal1;
 }
 //en modal til når spiller 2 vinder
 function player2WinsModal() {
@@ -264,15 +287,15 @@ function player2WinsModal() {
   player2WinsModal.style.display = "block";
 
   function closeModal2() {
-      player2WinsModal.style.display = "none";
-      window.location.href = "start.html";
+    player2WinsModal.style.display = "none";
+    window.location.href = "start.html";
   }
 
   closePlayer2WinsModal.addEventListener("click", closeModal2);
   window.addEventListener("click", function (event) {
-  if(event.target === player2WinsModal) {
+    if (event.target === player2WinsModal) {
       closeModal2();
-  }
+    }
   });
 
   const closeButton = document.querySelector(".closeModalButton2");
@@ -286,15 +309,15 @@ function player3WinsModal() {
   player3WinsModal.style.display = "block";
 
   function closeModal3() {
-      player3WinsModal.style.display = "none";
-      window.location.href = "start.html";
+    player3WinsModal.style.display = "none";
+    window.location.href = "start.html";
   }
 
   closePlayer3WinsModal.addEventListener("click", closeModal3);
   window.addEventListener("click", function (event) {
-  if(event.target === player3WinsModal) {
+    if (event.target === player3WinsModal) {
       closeModal3();
-  }
+    }
   });
 
   const closeButton = document.querySelector(".closeModalButton4");
@@ -308,15 +331,15 @@ function player4WinsModal() {
   player4WinsModal.style.display = "block";
 
   function closeModal4() {
-      player4WinsModal.style.display = "none";
-      window.location.href = "start.html";
+    player4WinsModal.style.display = "none";
+    window.location.href = "start.html";
   }
 
   closePlayer4WinsModal.addEventListener("click", closeModal4);
   window.addEventListener("click", function (event) {
-  if(event.target === player4WinsModal) {
+    if (event.target === player4WinsModal) {
       closeModal4();
-  }
+    }
   });
 
   const closeButton = document.querySelector(".closeModalButton5");
